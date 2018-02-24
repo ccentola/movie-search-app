0ea0c1e2fbb5869e1f5b3577bf3a4151a1f09afa
Carl Centola <carl.centola@gmail.com>
2018-02-24 10:11:23 -0500

    added results page

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 
 app.get("/results", function(req, res){
     request("http://omdbapi.com/?s=massachusetts&apikey=thewdb", function(error, response, body){
         if(!error && response.statusCode == 200){
-            var results = JSON.parse(body);
+            var data = JSON.parse(body);
 
             // return the title of the first movie
-            res.send(results["Search"][0]["Title"]);
+            res.render("results", {data: data});
             //console.log(parsedData)
         }
-    })
-})
+    });
+});
 
 /* app.listen(process.env.PORT, process.env.IP, function(){
     console.log("THE SERVER HAS STARTED");
